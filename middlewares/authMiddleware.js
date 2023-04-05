7c2da752f05c0a88dc13206f1709fc1cd9697d5f
vivek <vivek15292001@gmail.com>
2023-04-05 21:48:44 +0530

    day4-1

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ import JWT from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 
 // protected Routes token base
-export const requireSignIn = async (req,res,next) =>{
+export const requireSignIn = async (req, res, next) => {
     try {
         const decode = JWT.verify(req.headers.authorization, process.env.JWT_SECRET);
         req.user = decode;
@@ -16,21 +16,21 @@ export const requireSignIn = async (req,res,next) =>{
 //admin acceess
 export const isAdmin = async (req, res, next) => {
     try {
-      const user = await userModel.findById(req.user._id);
-      if (user.role !== 1) {
-        return res.status(401).send({
-          success: false,
-          message: "UnAuthorized Access",
-        });
-      } else {
-        next();
-      }
+        const user = await userModel.findById(req.user._id);
+        if (user.role !== 1) {
+            return res.status(401).send({
+                success: false,
+                message: "UnAuthorized Access",
+            });
+        } else {
+            next();
+        }
     } catch (error) {
-      console.log(error);
-      res.status(401).send({
-        success: false,
-        error,
-        message: "Error in admin middelware",
-      });
+        console.log(error);
+        res.status(401).send({
+            success: false,
+            error,
+            message: "Error in admin middelware",
+        });
     }
-  };
+};
